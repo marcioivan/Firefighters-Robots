d7520d0726a1306ca46d1ff262e1a2444efc0a36
gabrijob <gabrijob@gmail.com>
2018-11-13 12:38:11 -0200

    gitignore

---
+++ 2A_POO_robots2018/src/simulation/ChefPompierSimulation.java
@@ -0,0 +1,7 @@
+package simulation;
+
+public class ChefPompierSimulation {
+
+    public void introduceTo(RobotSimulation robot) {}
+    public void sendTaskTo(RobotSimulation robot) {}
+}
---
+++ 2A_POO_robots2018/src/simulation/RobotSimulation.java
@@ -0,0 +1,9 @@
+package simulation;
+
+import representation_donnees.Case;
+
+public class RobotSimulation {
+
+    private void getShortestWay() {}
+    private long timeToCross(Case square) {return 0;}
+}
