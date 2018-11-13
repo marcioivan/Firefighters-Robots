f446f8c3b5f72e644d882b8c461e0ab138340041
Gabriel Job Antunes Grabher <jobantug@ensipc558.ensimag.fr>
2018-11-13 16:38:17 +0100

    init robot simulation

--- 2A_POO_robots2018/src/simulation/RobotSimulation.java
+++ 2A_POO_robots2018/src/simulation/RobotSimulation.java
@@ -1,9 +1,26 @@
 package simulation;
 
+import representation_donnees.Carte;
 import representation_donnees.Case;
+import representation_donnees.Robot;
 
 public class RobotSimulation {
+    private Robot robot;
+    private Carte carte;
 
-    private void getShortestWay() {}
-    private long timeToCross(Case square) {return 0;}
+    public RobotSimulation(Robot r, Carte c) {
+        robot = r;
+        carte = c;
+    }
+
+    private double timeToCross(Case square) { // in seconds
+        return (robot.getVitesse(square.getNature()) * 1000)  / (carte.getTailleCases() * 3600);
+    }
+
+    private void getShortestWay(Case dst) {}
+
+    public void moveTo(Case dst) {
+        getShortestWay(dst);
+        //Create events
+    }
 }
